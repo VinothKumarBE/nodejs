e3d4915464628e49c0c4118407b4786689ed1cbe
Vinothkumar <t-vinoth@blocoville.com>
2023-02-18 00:02:26 +0530

    Connecting MongoDB and add a data to show in Mongo compass

--- Natours_API/server.js
+++ Natours_API/server.js
@@ -1,11 +1,56 @@
 /* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB =process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+.connect(DB,{
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() =>console.log('DB connection sucessfully!'))
+
  // eslint-disable-next-line no-console
 //console.log(process.env);
+
+
+const  tourSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true,'a tour  must have  a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price:{
+    type: Number,
+    required:[true, " A tour must have a price"]
+  }
+
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name:'the forset  hiker',
+  rating : 4.7,
+  price: 497
+ });
+
+testTour.save().then(doc =>{
+  console.log(doc);
+
+}).catch(err =>{
+  console.log('Error:',err)
+})
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
